7168d507980a87eb276c800a497f7048238ebef0
adikalon <webadev@yandex.ru>
2024-08-09 19:33:48 +0300

    Colored shulker

--- shulker.lua
+++ shulker.lua
@@ -1,19 +1,25 @@
---MCmobs v0.2
---maikerumine
---made for MC like Survival game
---License for code WTFPL and otherwise stated in readmes
-
--- intllib
 local MP = minetest.get_modpath(minetest.get_current_modname())
-local S, NS = dofile(MP.."/intllib.lua")
+local S, NS = dofile(MP .. "/intllib.lua")
 
---dofile(minetest.get_modpath("mobs").."/api.lua")
-
---###################
---################### SHULKER
---###################
-
--- animation 45-80 is transition between passive and attack stance
+local colors = {
+	"mobs_mc_shulker.png",
+	"mobs_mc_shulker_black.png",
+	"mobs_mc_shulker_blue.png",
+	"mobs_mc_shulker_brown.png",
+	"mobs_mc_shulker_cyan.png",
+	"mobs_mc_shulker_gray.png",
+	"mobs_mc_shulker_green.png",
+	"mobs_mc_shulker_light_blue.png",
+	"mobs_mc_shulker_lime.png",
+	"mobs_mc_shulker_magenta.png",
+	"mobs_mc_shulker_orange.png",
+	"mobs_mc_shulker_pink.png",
+	"mobs_mc_shulker_purple.png",
+	"mobs_mc_shulker_red.png",
+	"mobs_mc_shulker_silver.png",
+	"mobs_mc_shulker_white.png",
+	"mobs_mc_shulker_yellow.png",
+}
 
 mobs:register_mob("mobs_mc:shulker", {
 	type = "monster",
@@ -28,19 +34,26 @@ mobs:register_mob("mobs_mc:shulker", {
 	collisionbox = {-0.5, -0.01, -0.5, 0.5, 0.99, 0.5},
 	visual = "mesh",
 	mesh = "mobs_mc_shulker.b3d",
-	textures = { "mobs_mc_endergolem.png", },
-	-- TODO: Make shulker dye-able
-	visual_size = {x=3, y=3},
+	textures = { "mobs_mc_shulker.png", },
+	visual_size = {x = 2.7, y = 2.7},
 	walk_chance = 0,
 	jump = false,
 	drops = {},
 	animation = {
-		stand_speed = 25, walk_speed = 25, run_speed = 50, punch_speed = 25,
-		speed_normal = 25,		speed_run = 50,
-		stand_start = 0,		stand_end = 45,
-		walk_start = 0,		walk_end = 45,
-		run_start = 0,		run_end = 45,
-        punch_start = 80,  punch_end = 100,
+		stand_speed = 25,
+		walk_speed = 25,
+		run_speed = 50,
+		punch_speed = 25,
+		speed_normal = 25,
+		speed_run = 50,
+		stand_start = 0,
+		stand_end = 45,
+		walk_start = 0,
+		walk_end = 45,
+		run_start = 0,
+		run_end = 45,
+        punch_start = 80,
+		punch_end = 100,
 	},
 	blood_amount = 0,
 	view_range = 16,
@@ -48,13 +61,16 @@ mobs:register_mob("mobs_mc:shulker", {
 	water_damage = 1,
 	lava_damage = 4,
 	light_damage = 0,
+	on_spawn = function(self)
+		local color = math.random(1, #colors)
+		self.object:set_properties({textures = {colors[color]}})
+	end,
 })
 
--- bullet arrow (weapon)
 mobs:register_arrow("mobs_mc:shulkerbullet", {
 	visual = "sprite",
 	visual_size = {x = 0.25, y = 0.25},
-	textures = {"mobs_mc_shulkerbullet.png"},
+	textures = {"mobs_mc_shulker.png"},
 	velocity = 6,
 
 	hit_player = function(self, player)
@@ -78,8 +94,14 @@ mobs:register_arrow("mobs_mc:shulkerbullet", {
 
 mobs:register_egg("mobs_mc:shulker", S("Shulker"), "mobs_mc_spawn_icon_shulker.png", 0)
 
-mobs:spawn_specific("mobs_mc:shulker", mobs_mc.spawn.end_city, {"air"}, 0, minetest.LIGHT_MAX+1, 30, 5000, 2, mobs_mc.spawn_height.end_min, mobs_mc.spawn_height.end_max)
-
-if minetest.settings:get_bool("log_mods") then
-	minetest.log("action", "MC Shulkers loaded")
-end
+mobs:spawn({
+	name = "mobs_mc:shulker",
+	nodes = {"default:dirt_with_grass", "default:dirt_with_rainforest_litter", "default:dirt", "default:dirt_with_snow", "default:snow", "default:snowblock"},
+	neighbors = {"group:grass"},
+	min_light = 14,
+	interval = 60,
+	chance = 8000,
+	min_height = 5,
+	max_height = 200,
+	day_toggle = true
+})
