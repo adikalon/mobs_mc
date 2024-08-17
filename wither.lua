b25f3b9ecd607b6b257d51028b9280a2c210af5d
adikalon <webadev@yandex.ru>
2024-08-17 20:22:55 +0300

    Wither review

--- wither.lua
+++ wither.lua
@@ -1,24 +1,10 @@
---MCmobs v0.4
---maikerumine
---made for MC like Survival game
---License for code WTFPL and otherwise stated in readmes
-
 local S = minetest.get_translator(minetest.get_current_modname())
-
---dofile(minetest.get_modpath("mobs").."/api.lua")
-
-
---###################
---################### WITHER
---###################
-
 
 mobs:register_mob("mobs_mc:wither", {
 	type = "monster",
 	hp_max = 300,
 	hp_min = 300,
 	armor = 80,
-	-- This deviates from MC Wiki's size, which makes no sense
 	collisionbox = {-0.9, 0.4, -0.9, 0.9, 2.45, 0.9},
 	visual = "mesh",
 	mesh = "mobs_mc_wither.b3d",
@@ -42,10 +28,10 @@ mobs:register_mob("mobs_mc:wither", {
 	jump_chance = 98,
 	fly = true,
 	dogshoot_switch = 1,
-	dogshoot_count_max =1,
+	dogshoot_count_max = 1,
 	attack_animals = true,
-	floats=1,
-	drops = {},
+	floats = 1,
+	drops = mobs_mc.drops.wither,
 	water_damage = 0,
 	lava_damage = 0,
 	light_damage = 0,
@@ -58,82 +44,35 @@ mobs:register_mob("mobs_mc:wither", {
 	shoot_interval = 0.5,
 	shoot_offset = -1,
 	animation = {
-		walk_speed = 12, run_speed = 12, stand_speed = 12,
-		stand_start = 0,		stand_end = 20,
-		walk_start = 0,		walk_end = 20,
-		run_start = 0,		run_end = 20,
+		walk_speed = 12,
+		run_speed = 12,
+		stand_speed = 12,
+		stand_start = 0,
+		stand_end = 20,
+		walk_start = 0,
+		walk_end = 20,
+		run_start = 0,
+		run_end = 20,
 	},
 	blood_amount = 0,
 })
 
-local mobs_griefing = minetest.settings:get_bool("mobs_griefing") ~= false
-
-mobs:register_arrow("mobs_mc:roar_of_the_dragon", {
-	visual = "sprite",
-	visual_size = {x = 1, y = 1},
-	textures = {"blank.png"},
-	velocity = 10,
-
-	on_step = function(self, dtime)
-
-		local pos = self.object:getpos()
-
-		local n = minetest.get_node(pos).name
-
-		if self.timer == 0 then
-			self.timer = os.time()
-		end
-
-		if os.time() - self.timer > 8 or minetest.is_protected(pos, "") then
-			self.object:remove()
-		end
-
-		local objects = minetest.get_objects_inside_radius(pos, 1)
-	    for _,obj in ipairs(objects) do
-			local name = self.name
-			if name~="mobs_mc:roar_of_the_dragon" and name ~= "mobs_mc:wither" then
-		        obj:set_hp(obj:get_hp()-0.05)
-		        if (obj:get_hp() <= 0) then
-		            if (not obj:is_player()) and name ~= self.object:get_luaentity().name then
-		                obj:remove()
-		            end
-		        end
-			end
-	    end
-
-		if mobs_griefing then
-			minetest.set_node(pos, {name="air"})
-			if math.random(1,2)==1 then
-				local dx = math.random(-1,1)
-				local dy = math.random(-1,1)
-				local dz = math.random(-1,1)
-				local p = {x=pos.x+dx, y=pos.y+dy, z=pos.z+dz}
-				minetest.set_node(p, {name="air"})
-			end
-		end
-	end
-})
---GOOD LUCK LOL!
--- fireball (weapon)
-mobs:register_arrow(":mobs_mc:fireball", {
+mobs:register_arrow("mobs_mc:fireball", {
 	visual = "sprite",
 	visual_size = {x = 0.75, y = 0.75},
-	-- TODO: 3D projectile, replace tetxture
 	textures = {"mobs_mc_TEMP_wither_projectile.png"},
 	velocity = 6,
 
-	-- direct hit, no fire... just plenty of pain
 	hit_player = function(self, player)
-	minetest.sound_play("tnt_explode", {pos = pos, gain = 1.5, max_hear_distance = 16})
+		minetest.sound_play("tnt_explode", {pos = pos, gain = 1.5, max_hear_distance = 16})
 		player:punch(self.object, 1.0, {
 			full_punch_interval = 0.5,
 			damage_groups = {fleshy = 8},
 		}, nil)
-
 	end,
 
 	hit_mob = function(self, player)
-	minetest.sound_play("tnt_explode", {pos = pos, gain = 1.5,max_hear_distance = 16})
+		minetest.sound_play("tnt_explode", {pos = pos, gain = 1.5, max_hear_distance = 16})
 		player:punch(self.object, 1.0, {
 			full_punch_interval = 0.5,
 			damage_groups = {fleshy = 8},
@@ -141,14 +80,13 @@ mobs:register_arrow(":mobs_mc:fireball", {
 
 	end,
 
-	-- node hit, bursts into flame
 	hit_node = function(self, pos, node)
-		-- FIXME: Deprecated, switch to mobs:boom instead
 		mobs:explosion(pos, 3, 0, 1)
-	end
+	end,
 })
---Spawn egg
+
 mobs:register_egg("mobs_mc:wither", S("Wither"), "mobs_mc_spawn_icon_wither.png", 0)
 
---Compatibility
-mobs:alias_mob("nssm:mese_dragon", "mobs_mc:wither")
+if not mobs_mc.custom_spawn then
+	mobs:spawn(mobs_mc.spawns.wither)
+end
